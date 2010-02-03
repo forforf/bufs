b21f567bd18032c50eb9ae58989f66ae447f6f98
Dave M <dmarti21@gmail.com>
2010-02-03 19:49:13 +0000

    Added the ability to read directory structures into an array of read only input nodes that can (soon) fed into the AbstractNode sync engine

---
+++ spec/view_directory_reader_spec.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../bufs_fixtures/bufs_fixtures'
+CouchDB = BufsFixtures::CouchDB
+
+module ViewDirReaderSpec
+  LibDir = File.dirname(__FILE__) + '/../lib/'
+  FileReaderDir = File.dirname(__FILE__) + '/../sandbox_for_specs/view_dir_reader_spec/'
+end
+
+
+require ViewDirReaderSpec::LibDir + 'view_directory_reader'
+
+
+
+describe ViewDirectoryReader do
+
+  before(:all) do
+    @reader = ViewDirectoryReader.new(ViewDirReaderSpec::FileReaderDir)
+  end
+
+  #TODO Make this a real test rather than a manual inspection
+  it "should parse the directory" do
+    node_dir = @reader.read_directory
+    node_dir.each do |node|
+      puts "Node: #{node.my_category}"
+      puts " -- Type: #{node.class}"
+      puts " -- Parents: #{node.parent_categories.inspect}"
+      puts " -- Sub Entries: #{node.sub_entries.inspect}"
+      puts " --- File Metadata: #{node.file_metadata.inspect}"
+      puts " --- File Data: #{node.get_file_data.inspect}"
+    end
+  end
+end
