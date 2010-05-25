e197b419a777dcd101d325cd99a49d5bbe90299e
Dave M <dmarti21@gmail.com>
2010-05-25 20:41:21 +0000

    updated bufs_info_doc to handle deletion of individual attachments, still some clean up to do (updating specs and fixing the delete_attachments method -- maybe rename it since its too close to delete_attachment

--- lib/bufs_info_doc.rb
+++ lib/bufs_info_doc.rb
@@ -217,12 +217,24 @@ class BufsInfoDoc < CouchRest::ExtendedDocument
     current_node_attach.save
   end
 
-  def remove_attachment(attachment_name)
+  def remove_attachments(attachment_name)
+    #FIXME attachment name is meaningless here, also need specs for this
     current_node_doc = self.class.get(self['_id'])
     att_doc_id = current_node_doc.delete('attachment_doc_id')
     current_node_doc.save
     current_node_attach = self.class.user_attachClass.get(att_doc_id)
     current_node_attach.destroy
+  end
+
+
+  def remove_attachment(attachment_name)
+    #TODO: need specs
+    current_node_doc = self.class.get(self['_id'])
+    att_doc_id = current_node_doc['attachment_doc_id']
+    current_node_attachment_doc = self.class.user_attachClass.get(att_doc_id)
+    current_node_attachment_doc['md_attachments'].delete(attachment_name)
+    current_node_attachment_doc.delete_attachment(attachment_name)
+    current_node_attachment_doc.save
   end
 
   #Save the object to the CouchDB database
@@ -284,86 +296,3 @@ class BufsInfoDoc < CouchRest::ExtendedDocument
 
 end
 
-#=end
-=begin
-class UserDoc
-
-  def initialize(init_params = {})
-    init_params.each do |attr_name, attr_value|
-      iv_set(attr_name, attr_value)
-    end
-    @my_dir = BufsFileSystem.name_space + '/' + self.my_category + '/' if self.my_category
-    @attached_files = []
-  end
-
-  def iv_set(attr_var, attr_value)
-    instance_variable_set("@#{attr_var}", attr_value)
-  end
-
-end
-
-class UserDB
-  #attr_accessor :namespace
-  class << self; attr_accessor :user_to_docClass, :docClass_users, :docClasses; end
-  UserDB.docClasses = []
-  UserDB.user_to_docClass = {}
-  UserDB.docClass_users = {}
-
-  attr_reader :docClass, :namespace #, :user_attach_class_name
-  def initialize(couchdb, user_id)
-    @namespace = couchdb
-    @user_doc_class_name = "UserDoc#{user_id}"
-    @user_attach_class_name = "UserAttach#{user_id}"
-
-    #Security TODO: remove spaces and other 
-
-    #initialize Class and add constant for the User namespace
-    #---- Dynamic Class Definitions ----
-    dyn_user_class_def = "class #{@user_doc_class_name} < BufsInfoDoc
-      use_database CouchRest.database!(\"http://#{@namespace.to_s}/\")
-      class << self; attr_accessor :user_attachClass; end
-
-      #Find documents by their category
-      view_by :my_category
-
-      def self.namespace
-        CouchRest.database!(\"http://#{@namespace.to_s}/\")
-      end
-    end"
-
-    dyn_attach_class_def = "class #{@user_attach_class_name} < BufsInfoAttachment
-      use_database CouchRest.database!(\"http://#{@namespace.to_s}/\")
-
-      def self.namespace
-        CouchRest.database!(\"http://#{@namespace.to_s}/\")
-      end
-    end"
-
-    #----------------------------------
-    #puts "Dynamic Class Def:\n #{dyn_class_def}"
-
-    UserDB.class_eval(dyn_user_class_def)
-    UserDB.class_eval(dyn_attach_class_def)
-    #puts "Database: #{UserDB.const_get(@user_doc_class_name).use_database.inspect}"
-
-    @docClass = UserDB.const_get(@user_doc_class_name)
-    @attachClass = UserDB.const_get(@user_attach_class_name)
-    @docClass.user_attachClass = @attachClass
-
-    #Perform user <=> CouchDB Document bindings
-    #Add to List of docClasses
-    UserDB.docClasses << @docClass
-    UserDB.docClasses.uniq!
-    #Assign user CouchDB Document (for looking up user's docClass)
-    UserDB.user_to_docClass[user_id] = @docClass
-    #Assign users to a CouchDB Extended Document Class (allows shared db for multiple users)
-    if UserDB.docClass_users[@docClass.name]
-      UserDB.docClass_users[@docClass.name]  << user_id
-    else
-      UserDB.docClass_users[@docClass.name] = [user_id]
-    end
-    UserDB.docClass_users[@docClass.name].uniq!  
-
-  end
-end 
-=end 
