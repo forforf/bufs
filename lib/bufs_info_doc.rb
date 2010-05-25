a12ce7f79bae1eb9e956c3ccd2f29a04b4864671
Dave M <dmarti21@gmail.com>
2010-05-25 21:21:13 +0000

    added the ability to get an attachment url from the bufs_info_doc

--- lib/bufs_info_doc.rb
+++ lib/bufs_info_doc.rb
@@ -237,6 +237,13 @@ class BufsInfoDoc < CouchRest::ExtendedDocument
     current_node_attachment_doc.save
   end
 
+  def attachment_url(attachment_name)
+    current_node_doc = self.class.get(self['_id'])
+    att_doc_id = current_node_doc['attachment_doc_id']
+    current_node_attachment_doc = self.class.user_attachClass.get(att_doc_id)
+    current_node_attachment_doc.attachment_url(attachment_name)
+  end
+
   #Save the object to the CouchDB database
   #  save_type can be either :additions or :deletions
   #  :additions will merge parent categories with any categories in the database
