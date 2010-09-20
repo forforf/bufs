1bc073c05495b8ca8c7a32dbd6590fd72b3c18bf
Dave M <dmarti21@gmail.com>
2010-09-20 14:44:56 +0000

    Tree view WIP

--- spec/bufs_node_factory_spec.rb
+++ spec/bufs_node_factory_spec.rb
@@ -1,89 +1,22 @@
+DirBufsNodeFactorySpec = File.dirname(__FILE__)
+require File.join(DirBufsNodeFactorySpec, 'helpers/bufs_test_environments')
 
-require File.dirname(__FILE__) + '/../bufs_fixtures/bufs_fixtures'
+#Test Environments
+#The test environments sets up the following:
+# 4 seperate persistent layers
+# - 2 CouchDB databases using CouchRest 
+# - 2 File Systems (just different directories)  using File and FileUtils
+# A data structure called Bufs
+# 2 "Glue" enviroments that bind the persistent layers to the data structure
+# The 4 low level persistent layers  are mapped to a common model framework
+# using the glue environments to bind the datastructure and persistent layers 
+# to the framework
 
-
-require 'couchrest'
-node_db_name = "http://bufs.younghawk.org:5984/bufs_test_spec/"
-CouchDB = BufsFixtures::CouchDB #CouchRest.database!(node_db_name)
-CouchDB.compact!
-CouchDB2 = BufsFixtures::CouchDB2
-CouchDB2.compact!
-FileSystem1 = "/home/bufs/bufs/sandbox_for_specs/file_system_specs/group1"
-FileSystem2 = "/home/bufs/bufs/sandbox_for_specs/file_system_specs/group2"
-
-require File.dirname(__FILE__) + '/../lib/bufs_node_factory'
-
-module UserNodeSpecHelpers
-  BufsNodeLibs = [File.dirname(__FILE__) + '/../lib/glue_envs/bufs_couchrest_glue_env']
-  BufsNodeIncludes = [:CouchRestEnv]
-  BufsFileLibs = [File.dirname(__FILE__) + '/../lib/glue_envs/bufs_filesystem_glue_env']
-  BufsFileIncludes = [:FileSystemEnv]
-end
-
-#for testing CouchRest model
-module CouchRestNodeHelpers
-
-  def self.env_builder(node_class_id, db, db_user_id)
-      node_env = Hash[ node_class_id =>
-                      Hash[ :requires => UserNodeSpecHelpers::BufsNodeLibs,
-                            :includes => UserNodeSpecHelpers::BufsNodeIncludes,
-                            :glue_name => "BufsCouchRestEnv",
-                            :class_env =>
-                            Hash[ :bufs_info_doc_env =>
-                                  Hash[ :host => db.host,
-                                        :path => db.uri,
-                                        :user_id => db_user_id 
-                                      ]
-                                ]
-                          ]
-                    ]
-  end
-end
-
-module FileSystemNodeHelpers
-  def self.env_builder(node_class_id, root_path, fs_user_id)
-      node_env = Hash[ node_class_id =>
-                      Hash[ :requires => UserNodeSpecHelpers::BufsFileLibs,
-                            :includes => UserNodeSpecHelpers::BufsFileIncludes,
-                            :glue_name => "BufsFileSystemEnv",
-                            :class_env =>
-                            Hash[ :bufs_file_system_env =>
-                                  Hash[ :path => root_path,
-                                        :user_id => fs_user_id
-                                      ]
-                                ]
-                          ]
-                    ]
-  end
-
-end
-
-module NodeHelpers  
-  DefaultNodeParams = {:my_category => 'default',
-                      :parent_categories => ['default_parent'],
-                      :description => 'default description'}
-
-  def get_default_params
-    DefaultNodeParams.dup #to avoid a couchrest weirdness don't use the params directly
-  end
-
-  def make_doc_no_attachment(user_class, override_defaults={})
-    init_params = get_default_params.merge(override_defaults)
-    return user_class.new(init_params)
-  end
-
-  def make_doc_w_attach_from_file(user_class, att_fname, override_defaults={})
-    test_filename = att_fname 
-    test_basename = File.basename(test_filename)
-    raise "can't find file #{test_filename.inspect}" unless File.exists?(test_filename)
-    new_doc = make_doc_no_attachment(user_class, override_defaults)
-    new_doc.__save #doc must be saved before we can attach
-    file_data = {:src_filename => test_filename}
-    new_doc.files_add(file_data)
-    #new_doc.add_data_file(test_filename)
-    return new_doc 
-  end
-end
+#Node Helpers
+# make_doc_no_attachment - makes a common framework node without attachment
+#   arguments: user_class, parameters
+# make_doc_w_attach_from_ile - makes a node with the file as an attachment
+#   arguments: user_class, filename, parameters)
 
 module MakeUserClasses
     @user1_id = "CouchUser001"
@@ -102,33 +35,11 @@ module MakeUserClasses
     User2Class =  BufsNodeFactory.make(node_env2)
     User3Class =  BufsNodeFactory.make(node_env3)
     User4Class =  BufsNodeFactory.make(node_env4)
-    #@user_classes = [@user1_class, @user2_class, @user3_class, @user4_class]
 end
 
 describe BufsNodeFactory, "Making the Class" do
   include MakeUserClasses
-  #include BufsInfoNodeSpecHelpers
-=begin
-  before(:all) do
-    @user1_id = "CouchUser001"
-    @user2_id = "CouchUser002"
-    @user3_id = "FileSysUser003"
-    @user4_id = "FileSysUser004"
-    node_class_id1 = "BufsInfoNode#{@user1_id}"
-    node_class_id2 = "BufsInfoNode#{@user2_id}"
-    node_class_id3 = "BufsFile#{@user3_id}"
-    node_class_id4 = "BufsFile#{@user4_id}"
-    node_env1 = CouchRestNodeHelpers.env_builder(node_class_id1, CouchDB, @user1_id)
-    node_env2 = CouchRestNodeHelpers.env_builder(node_class_id2, CouchDB2, @user2_id)
-    node_env3 = FileSystemNodeHelpers.env_builder(node_class_id3, FileSystem1, @user3_id)
-    node_env4 = FileSystemNodeHelpers.env_builder(node_class_id4, FileSystem2, @user4_id)
-    @user1_class = BufsNodeFactory.make(node_env1) 
-    @user2_class = BufsNodeFactory.make(node_env2)
-    @user3_class = BufsNodeFactory.make(node_env3)
-    @user4_class = BufsNodeFactory.make(node_env4)
-    @user_classes = [@user1_class, @user2_class, @user3_class, @user4_class]
-  end
-=end
+
   before(:each) do
     @user_classes = [User1Class, User2Class, User3Class, User4Class]
   end
