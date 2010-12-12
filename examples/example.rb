dbad80e89789d7005aaab77f6fbd1d64857836ae
Dave M <dmarti21@gmail.com>
2010-12-12 21:04:55 +0000

    Updated examples to highlight the capabilities of the library 	modified:   examples/example.rb

--- examples/example.rb
+++ examples/example.rb
@@ -1,6 +1,6 @@
 require '../lib/bufs_node_factory'  #<-- eventually will be gem 'bufs'
 require '../lib/moabs/moab_couchrest_env'
-
+require '../lib/midas/node_element_operations'
 
 #We need to define the datastructure we'll be starting with.  We can change it dynamically as well,
 #but it is usually helpful to have a defined base to start from
@@ -28,106 +28,6 @@ module DataStructureModels
     RequiredSaveKeys = [:id]  #duplicative?
     NodeKey = :id #TODO look at supporting multiple node keys
   end
-end
-
-module ExampleDataStructure
-   
-  #We define a field that cannot be modified  #TODO: can this be defaulted??
-  StaticFieldAddOp = lambda{|this, other| Hash[:update_this => this] }
-  StaticFieldSubtractOp = lambda{|this, other| Hash[:update_this => this]}
-
-  StaticFieldOps = {:add => StaticFieldAddOp, :subtract => StaticFieldSubtractOp}
-  
-  
-  #We define a field where adding will replace the existing value for that field, and subtracting a matching value will set the value to nil
-  ReplaceFieldAddOp = lambda {|this, other|
-                                this = other 
-                                Hash[:update_this => this]
-                           }
-                           
-  ReplaceFieldSubtractOp = lambda {|this, other|
-                                        this = nil if (this == other)
-                                        Hash[:update_this => this]
-                                  }
-                                  
-  ReplaceFieldOps = {:add => ReplaceFieldAddOp, :subtract => ReplaceFieldSubtractOp}
-  #We define a field where adding will add the value to the existing list, and subtracting will remove matching values from the list
-  ListFieldAddOp = lambda {|this,other|
-                           this = this || []
-                           other = other || []
-                           this = this + [other].flatten
-                           this.uniq!; this.compact!
-                           Hash[:update_this => this]
-                         }
-                         
-  ListFieldSubtractOp = lambda {|this,other| 
-                                this = [this] || []
-                                other = [other] || []
-                                this.flatten!
-                                other.flatten!
-                                this -= other
-                                this.uniq!
-                                this.compact!
-                                Hash[:update_this => this]
-                               }
-  ListFieldOps = {:add => ListFieldAddOp, :subtract => ListFieldSubtractOp}
-  
-  #A bit more complicated is if we have a field that holds key-value pairs, but we want our operations
-  #to operate on the underlying values of the key-value pair, and not on the actual key value sets.
-  #Here the values are a list type.  What happens is if an existing key is passed, the value is added to the 
-  #set of values for the existing key.  If a new key is passed, the new key and its value are added to the list
-  KVListValAddOp = lambda {|this, other|
-                                 this = this || {}  
-                                 other = other || {}
-                                 okeys = other.keys
-                                 okeys.each {|k| if this[k]
-                                                    this[k] = [this[k] ].flatten + [ other[k] ].flatten
-                                                  else
-                                                    this[k] = [ other[k] ].flatten
-                                                  end 
-                                                  this[k].uniq!
-                                                  this[k].compact! 
-                                                  Hash[:update_this => this] }
-                                      }
-                                                  
-  KVListValSubtractOp = lambda {|this, other|
-                                                  this = this || {}
-                                                  #Hacked together needs thought out (and TESTED!!)
-                                                  other = other || {}
-                                                  puts "This / Other: #{this.inspect} / #{other.inspect}"
-                                                  #srcs = [other].flatten
-                                                  other.keys.each do |k|
-                                                      #other[s].each {|olnk| this[k].delete(olnk) if this[k]}
-                                                      puts "delete #{other[k].inspect} from #{this[k].inspect}"
-                                                      #this[k].delete(other[k]) if this[k]
-                                                      this.delete(k) 
-                                                      #this.delete(k) if (this[k].nil? || this[k].empty?)
-                                                  end
-                                                  Hash[:update_this => this]
-                                            }
-  # With the KVP, we might want the keys that contain a given value
-  #note that in this case, the return value is not the same as the value stored in the field, hence the explicit return_value parameter
-  KVPGetKeyforValueOp = lambda {|this, value|
-                                                this = this|| {}
-                                                keys = []
-                                                this.each{ |k,v| keys << k if v.include? value }
-                                                rtn_val = if srcs.size > 1
-                                                                {:return_value => keys, :update_this => this}
-                                                              else
-                                                                {:return_value => nil, :update_this => this}
-                                                              end
-                                                rtn_val
-                                              }
-
-  KVListOps = {:add => KVListValAddOp, :subtract => KVListValSubtractOp, :get_keys => KVPGetKeyforValueOp}
-
-  #the keys represent the data type, the values represent the operations to perform on those datatypes  
-  Ops = {:id => StaticFieldOps, :label => ReplaceFieldOps, :tags => ListFieldOps, :kvps=> KVListOps}
-end
-
-#UGLY UGLY HACK to get around defiency in Bufs library
-module NodeElementOperations
-  Ops = ExampleDataStructure::Ops
 end
 
 #TODO: Move these into the main libs.
@@ -422,7 +322,12 @@ end
   #TODO: It might be better if the node_class_id should be defaulted to the user name (or derivative)
   node_class_id = :MyExample
   reqs = nil #we aren't using an external file to hold the modules to be included
-  incls = [ExampleDataStructure]
+  
+  #changing default configuration
+  incls = [NodeElementOperations] #[ExampleDataStructure]
+
+  include NodeElementOperations
+  NodeElementOperations.configuration =  {:id => StaticFieldOps, :label => ReplaceFieldOps, :tags => ListFieldOps, :kvps=> KVListOps} 
   user_id = "Me"
   couch_env = CouchRestNodeHelpers.env_builder(node_class_id, reqs, incls, couchrest_instance, user_id)
   #p couch_env
@@ -435,6 +340,32 @@ end
   puts "Node in CouchDB"
   #p hello_world_node
   p couchrest_instance.get(hello_world_node._model_metadata[:_id])
+  puts
   puts "Or you can test it from the command line using curl:"
   puts "curl -X GET #{example_couchdb_location}/#{CGI.escape(hello_world_node._model_metadata[:_id])}"
-
+  puts
+  puts "We can also add a field dynamically, for example a \"tags\" field"
+  puts "Node after dynamically adding new data element"
+  hello_world_node.__set_userdata_key(:tags, ["tag1", "tag2"])
+  p hello_world_node._user_data
+  puts "You don't have to add a field defined in the element operations"
+  puts "the \"random_field\" will behave (how? what is the default behavior?"
+  #TODO, if no value provided, default to nil (no reason to force user to set it)
+  #TODO: Raise an appropriate error if an operation tag is left off (add, subtract, etc)
+  #TODO: Provide a default operation set for unspecified tags
+  #TODO: Also allow user to set the default  operation to use for undefined tags
+  hello_world_node.__set_userdata_key(:random_field, nil)
+  p hello_world_node._user_data
+  puts
+  puts "Node Element Operations in action"
+  puts "Lets Add \"WontAdd\" to the id field"
+  puts "and \"A New Hello World\" to the label field"
+  puts "and \"tag3\" to the tags field"
+  puts "and \"random\" to the random_field field (well not yet, it creates an error)"
+  hello_world_node.id_add "WontAdd"
+  hello_world_node.label_add "A New Hello World"
+  hello_world_node.tags_add "tag3"
+  #hello_world_node.random_field_add "random"
+  p hello_world_node._user_data
+  puts
+  puts "Ok, now lets subtract (later)"
