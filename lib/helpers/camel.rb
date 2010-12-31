bfd948a124632354871191d243a63d766fef82a1
Dave M <dmarti21@gmail.com>
2010-12-31 21:38:49 +0000

    Adding Camel.score to complement Camel.ize

--- lib/helpers/camel.rb
+++ lib/helpers/camel.rb
@@ -8,4 +8,14 @@ module Camel
       lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
     end
   end
+  
+  def underscore(camel_cased_word)
+     word = camel_cased_word.to_s.dup
+     word.gsub!(/::/, '/')
+     word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+     word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+     word.tr!("-", "_")
+     word.downcase!
+     word
+  end
 end
